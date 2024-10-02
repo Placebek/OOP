a67f73c42895e74b75341d6ae8452488f3c512fb
Placebek <goblins.from.mars2003@gmail.com>
2024-10-02 15:49:57 +0500

    Настроил иконку, установил bootstrap-icons просто так

--- src/components/MusicList.jsx
+++ src/components/MusicList.jsx
@@ -53,19 +53,22 @@ const MusicList = (props) => {
             {/* Большое окно */}
             <CSSTransition
                 in={isOpen}
-                timeout={500}
+                timeout={200}
                 classNames="modal"
                 appear
                 unmountOnExit
             >
                 <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-end">
                     <div className="bg-[#504e4a] w-full h-[580px] rounded-t-2xl relative flex justify-center">
+                        <button className="h-20 w-20 absolute left-1 ml-4
+                        " onClick={closeWindow}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="white" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708" />
+                            </svg>
+                        </button>
                         <div className="h-[268px] w-[268px] mt-10">
                             <img className='rounded-sm' src={props.post.img} alt="Картина песни" />
                         </div>
-                        <button className="h-20 w-20 absolute " onClick={closeWindow}>
-                            <img className="" src="" alt="Назад" />
-                        </button>
                     </div>
                 </div>
             </CSSTransition>
@@ -91,14 +94,14 @@ const MusicList = (props) => {
                             {props.post.authorName}
                         </div>
                     </div>
-                    <div className="ml-auto mr-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke={checkLike} className="size-8">
+                    <div className="ml-auto mr-6 mt-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke={checkLike} className="size-10">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                         </svg>
+                        <button onClick={closeSmallWindow} className="mr-6">
+                            <img src="" alt="" />fff
+                        </button>
                     </div>
-                    <button onClick={closeSmallWindow} className="">
-                        <img src="" alt="" />
-                    </button>
                 </div>
             </CSSTransition>
 
