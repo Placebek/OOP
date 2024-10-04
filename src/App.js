9897dcde9afc850a21bce0f1615d7a24a3ac21b1
Placebek <goblins.from.mars2003@gmail.com>
2024-10-04 14:44:13 +0500

    Декомпозиция, перераспределил код, добавляю фиксированную навигатор

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Tanymal from './components/Tanymal'
 import Header from './components/main/Header'
 import MusicList from './components/MusicList'
 import Search from './components/Search'
+import Menu from './components/main/Menu'
 import './index.css'
 import Juzz from '../src/templates/icons/Juzz.jpeg'
 import Dom50Logo from '../src/templates/icons/Дом 50.jpeg'
@@ -10,18 +11,21 @@ import Dom50Logo from '../src/templates/icons/Дом 50.jpeg'
 function App() {
 	const [musics, setMusicList] = useState([
 		{
+			id: 1,
 			img: Juzz,
 			title: 'Sheker',
 			authorName: 'TBRN',
 			liked: false,
 		},
 		{
+			id: 2,
 			img: Dom50Logo,
 			title: 'Дом 50',
 			authorName: 'NOVЫЙ',
 			liked: true,
 		},
 		{
+			id: 3,
 			img: Juzz,
 			title: 'Uide',
 			authorName: 'TBRN',
@@ -30,13 +34,18 @@ function App() {
 	])
 
 	return (
-		<div className='{} * : h-screen bg-gradient-to-b from-[#FFFFFF] to-[#999999] '>
-			<Header />
-			<Search />
-			<Tanymal />
-			{musics.map(post => (
-				<MusicList post={post} />
-			))}
+		<div className='flex flex-col min-h-screen bg-gradient-to-b from-[#FFFFFF] to-[#999999]'>
+			<div className='flex-grow'>
+				<Header />
+				<Search />
+				<Tanymal />
+
+				{musics.map(post => (
+					<MusicList post={post} key={post.id} />
+				))}
+			</div>
+
+			<Menu />
 		</div>
 	)
 }
--- src/components/MusicList.jsx
+++ src/components/MusicList.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
-import { CSSTransition } from "react-transition-group";
 import './ModalAnimations.css';
+import MusicModalWindow from "./modals/MusicModalWindow";
+import SmallWindow from "./modals/MusicSmallModalWindow";
 
 const MusicList = (props) => {
     const [isOpen, setCheckOpen] = useState(false);
     const [isOpenSmallWindow, setSmallWindow] = useState(false);
 
     const openWindow = () => {
-        setCheckOpen(true);
-        setSmallWindow(false);
+        if (!isOpenSmallWindow) {
+            setCheckOpen(true);
+        }
     };
 
     const closeSmallWindow = () => {
@@ -19,7 +21,7 @@ const MusicList = (props) => {
         setCheckOpen(false);
         setTimeout(() => {
             setSmallWindow(true);
-        }, 700);
+        }, 300);
     };
 
     const checkLike = props.post.liked ? '#FF7373' : 'currentColor';
@@ -48,63 +50,8 @@ const MusicList = (props) => {
                 </svg>
             </div>
 
-
-
-            {/* Большое окно */}
-            <CSSTransition
-                in={isOpen}
-                timeout={200}
-                classNames="modal"
-                appear
-                unmountOnExit
-            >
-                <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-end">
-                    <div className="bg-[#504e4a] w-full h-[580px] rounded-t-2xl relative flex justify-center">
-                        <button className="h-20 w-20 absolute left-1 ml-4
-                        " onClick={closeWindow}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="white" class="bi bi-chevron-down" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708" />
-                            </svg>
-                        </button>
-                        <div className="h-[268px] w-[268px] mt-10">
-                            <img className='rounded-sm' src={props.post.img} alt="Картина песни" />
-                        </div>
-                    </div>
-                </div>
-            </CSSTransition>
-
-
-
-            {/* Маленькое окно */}
-            <CSSTransition
-                in={isOpenSmallWindow}
-                timeout={200}
-                classNames="small-modal"
-                unmountOnExit
-            >
-                <div className="fixed bottom-0 left-0 right-0 bg-[#ffffff] h-[75px] mb-[75px] flex flex-nowrap">
-                    <div className="h-[75px] w-[75px]">
-                        <img className='' src={props.post.img} alt="Картина песни" />
-                    </div>
-                    <div className="ml-[26px] flex flex-col mt-3">
-                        <div className="pb-0 text-[22px] font-slim text-gray-800">
-                            {props.post.title}
-                        </div>
-                        <div className="text-[12px] text-gray-700 font-light">
-                            {props.post.authorName}
-                        </div>
-                    </div>
-                    <div className="ml-auto mr-6 mt-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke={checkLike} className="size-10">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                        </svg>
-                        <button onClick={closeSmallWindow} className="mr-6">
-                            <img src="" alt="" />fff
-                        </button>
-                    </div>
-                </div>
-            </CSSTransition>
-
+            <MusicModalWindow props={props} isOpen={isOpen} isOpenSmallWindow={isOpenSmallWindow} closeWindow={closeWindow} />
+            <SmallWindow props={props} isOpenSmallWindow={isOpenSmallWindow} closeSmallWindow={closeSmallWindow} checkLike={checkLike} />
 
         </div>
     );
