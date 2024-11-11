80b604a708e96d2521583a0b6534be4dcbcfd89a
Placebek <goblins.from.mars2003@gmail.com>
2024-11-12 01:14:08 +0500

    Добавлено Скроллинг в странице новостей

--- src/components/news/NewsPage.jsx
+++ src/components/news/NewsPage.jsx
@@ -23,18 +23,38 @@ const NewsPage = () => {
             unmountOnExit
             nodeRef={nodeRef}
         >
-            <div ref={nodeRef} className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-end">
-                <div className="bg-[#504e4a] w-full h-full fixed rounded-t-2xl flex justify-center">
-                    <Link onClick={() => dispatch(closeNewsWindow())} to='/news' className="h-10 w-10 absolute left-1 ml-4 mt-5">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="white" className="bi bi-chevron-down" viewBox="0 0 16 16">
-                            <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708" />
-                        </svg>
-                    </Link>
-                    <div className="">
+            <div ref={nodeRef} className="">
+                <div className="bg-[#504e4a] w-full h-full min-h-screen bg-diagonal-gradient flex justify-center">
+                    <div>
+                        <div
+                            key={props.id}
+                            style={{
+                                backgroundImage: `linear-gradient(180deg, rgba(98, 98, 98, 0.35) 50%, #000000 100%), url(${props.news_logo})`,
+                            }}
+                            className='h-[35vh] mt-1 rounded-lg bg-cover bg-center relative '
+                        >
+                            <div className="">
+                                <Link onClick={() => dispatch(closeNewsWindow())} to='/news' className="h-10 w-10 absolute left-1 ml-3 mt-3">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="white" class="size-8">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                                    </svg>
 
-                        <img className='mt-[60px] rounded-sm h-[268px] w-[268px]' src={props.news_logo} alt="Картина песни" />
-                        <p className="text-center mt-4 text-3xl text-white">{props.news_title}</p>
-                        <p className="text-center mt-1 text-sm font-extralight text-white">{props.news_text}</p>
+                                </Link>
+                                <div className="absolute font-medium top-12 left-3 text-[14px] text-white">{props.news_title}</div>
+                            </div>
+                            <div className="absolute bottom-2 left-3 text-white ">
+                                {props.news_author}
+                            </div>
+                            <div className="absolute bottom-2 right-3 text-white ">
+                                {props.news_created_date}
+                            </div>
+                        </div>
+                        <div className="w-full h-[12vh] bg-[#69637f7d] rounded-[19px]">
+                            <p className=" mt-3 p-3 text-[12px] text-white">{props.news_title}</p>
+                        </div>
+                        <div className="w-full max-h-[34vh] bg-[#69637f7d] rounded-[19px] overflow-y-auto mt-2 overscroll-none rtl">
+                            <p className="mt-1 text-sm font-light text-white mx-3">{props.news_text}</p>
+                        </div>
                     </div>
                 </div>
             </div>
